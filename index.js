aad3b374c967d664ad557485ebc4c2093101cc7f
IrynaRK <105289016+IrynaRK@users.noreply.github.com>
2024-11-10 18:31:23 +0100

    complit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 //index
+const input = document.querySelector("#input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", changeValue);
+
+function changeValue() {
+  input.value = "QWERTY";
+}
